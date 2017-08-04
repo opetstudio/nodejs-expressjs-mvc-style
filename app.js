ae670e3cf40ef88f6b72167efe7b42c165568426
Nofrets <opetstudio@gmail.com>
2017-08-04 17:04:17 +0700

    install module path

---
+++ app.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var config = require ('./config');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
